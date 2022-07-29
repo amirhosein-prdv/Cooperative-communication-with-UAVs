74138d45228b6d49d80f5e92e4565be3c3426ca6
amirhosein <ah.pourdavoud79@gmail.com>
2022-07-29 20:46:21 +0430

    CDF function of gamma and calculate throughput

---
+++ UAV Selection/gamma_CDF.m
@@ -0,0 +1,27 @@
+function out = gamma_CDF(gam, gamma_hat_AU ,gamma_hat_UB, m_AU, m_UB)
+
+    K = @(t, p) besselk(t-p+1, 2*sqrt((gam^2)/(gamma_hat_AU*gamma_hat_UB)));
+
+    x = 2*exp(-gam*(1/gamma_hat_AU + 1/gamma_hat_UB));
+    xx = 0;
+
+    for p = 0: m_AU-1
+        for t = 0: p + m_UB-1
+
+            v = ((gam^(p+m_UB))*(gamma_hat_UB^((t-p-2*m_UB+1)/2)))/...
+                ((gamma_hat_AU^((p+t+1)/2))*factorial(p)*gamma(m_UB));
+            xx = xx + nchoosek(p+m_UB-1, t) * v * K(t, p);
+    %         disp('p= ')
+    %         disp(p);
+    %         disp('t= ')
+    %         disp(t);
+
+        end
+    end
+
+
+    out = 1 - x * xx;
+    % out = xx;
+    
+end
+
